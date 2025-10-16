9496338ef271d8c30b79a15ccf8890c44a803ec4
Opilane TTHK <opilane@tthk.ee>
2025-10-16 10:18:51 +0300

    Switch

---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta täht ja vajuta enter");
+
+
+            string input = Console.ReadLine();
+            
+            switch (input)
+            {
+                case "a":
+                 Console.WriteLine("Sisestasid tähe a");
+                    break;
+
+                case "e":
+                    Console.WriteLine("sisestasid tähe e");
+                    break;
+
+                case "i":
+                    Console.WriteLine("sisestasid tähe i");
+                    break;
+
+                default:
+                    Console.WriteLine("Ei ole vokaal");
+                    break;
+            }
+        }
+    }
+}
