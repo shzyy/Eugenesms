4e901b5b84439d65c054546393d21bd3a612c8af
Opilane TTHK <opilane@tthk.ee>
2025-11-13 11:18:56 +0200

    switchbetweennumber

---
+++ switchbetweennumber/switchbetweennumber/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace switchbetweennumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta number");
+
+            int number = int.Parse (Console.ReadLine());
+
+            switch (number)
+            {
+                case >= 0 and <= 100:
+                    Console.WriteLine("sisestasid number (0-100)");
+                    break;
+
+                case >= 101 and <= 200:
+                    Console.WriteLine("sisestasid number (101-200)");
+                    break;
+
+                case >= 201 and <= 300:
+                    Console.WriteLine("sisestasid number (201-300)");
+                    break;
+
+                default:
+                    Console.WriteLine("Error");
+                    break;
+
+
+            }
+        }
+    }
+}
