15b09b8a09bf98bb9120c50d8561810f7683bc4f
Opilane TTHK <opilane@tthk.ee>
2025-11-20 09:53:00 +0200

    e

---
+++ SwitchTryCatch/SwitchTryCatch/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace SwitchTryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Try Catch koos Switch-ga");
+
+
+            //alguses teha switch kus saab sisestada numbreid 1 ja 2
+
+            int input = int.Parse(Console.ReadLine());
+
+            try
+            { 
+
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("sisestatud number 1");
+                    break;
+
+                    case 2:
+                    Console.WriteLine("sisestatud number 2");
+                    break;
+
+                    default:
+                    Console.WriteLine("Error");
+                    break;
+                }
+            }
+            catch (FormatException)
+            { 
+                Console.WriteLine("Palun sisesta kehtiv number");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Sisestatud number in liiga suur või liiga väike");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Tekkis ootamatu viga:{ex.Message}");
+            }
+        }
+    }
+
+}
