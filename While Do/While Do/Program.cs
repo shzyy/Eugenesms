f4b24b4be5be4c02f809723943b8c6d94b93f10e
Opilane TTHK <opilane@tthk.ee>
2025-11-20 10:36:27 +0200

    wd

---
+++ While Do/While Do/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace While_Do
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("while Do");
+
+            int i = 1;
+
+            do
+            {
+                Console.WriteLine(i);
+                //i++ tahendab, et sama mis i = i + i
+                i++;
+            }
+            while (i <= 10);
+            //kui tuli nr 11, siis tingimus ei kehti enam ja tsükkel lõpeb
+        }
+    }
+}
