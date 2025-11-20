d18c4493d8dec36e9ffacf82d8807bcf1b5e0a28
Opilane TTHK <opilane@tthk.ee>
2025-11-20 11:27:33 +0200

    Triangle

---
+++ TriangleWhileDo/TriangleWhileDo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TriangleWhileDo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolmnurk");
+
+            int n = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (i <= n)
+            {
+                Console.WriteLine();
+                int j = 1;
+                while (j <= i)
+                {
+                    Console.Write("*" + " ");
+                    j++;
+                }
+                i++;
+                {
+                    Console.ReadKey();
+
+            }
+        }
+    }
+    }
+}
+
