bec9ef0556d3527b870bfdc79a39ea45b12ab1f5
Opilane TTHK <opilane@tthk.ee>
2025-11-27 09:24:55 +0200

    while

---
+++ WhileNesting/WhileNesting/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace WhileNesting
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("While nesting");
+
+            int a = 1;
+
+            while (a <= 5) 
+            {
+                int b = 1;
+
+                while (b <= 5) 
+                {
+                    Console.Write(b + " ");
+                    b++;
+                }
+
+                Console.WriteLine();
+                a++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
