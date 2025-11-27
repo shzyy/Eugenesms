d2bf738cae1484face4a76cc5e373d81d1c3afb3
Opilane TTHK <opilane@tthk.ee>
2025-11-27 10:40:12 +0200

    ring

---
+++ calculator2/calculator2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace calculator2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kujundi kalkulaator");
+
+            int roundRadius = 3;
+
+            Console.WriteLine("Ruudu Pindala: " + (2 * roundRadius) * (2 * roundRadius));
+            Console.WriteLine("Ruudu ümbermõõt:" + (2 * roundRadius) * 4);
+            Console.WriteLine("Ruudu ümbermõõt" + (Math.PI *  roundRadius) * (2 * roundRadius));
+            Console.WriteLine("Ringi pindala:" + (Math.PI *  Math.PI) * (2 * Math.PI));
+                
+
+
+
+        }
+    }
+}
