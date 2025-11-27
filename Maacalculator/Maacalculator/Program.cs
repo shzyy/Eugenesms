67101bb88e2ade79b45aab60ace4c2ba6e19f0d0
Opilane TTHK <opilane@tthk.ee>
2025-11-27 10:58:01 +0200

    coin

---
+++ Maacalculator/Maacalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Maacalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator");
+
+            int coinDiameter = 26;
+            int earthRadius = 6371; 
+
+            Console.WriteLine("Maa ümbermõõt: " + (2 * Math.PI * earthRadius));
+            Console.WriteLine("Müntide arv ümber Maa: " + (2 * Math.PI * earthRadius) / coinDiameter);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
