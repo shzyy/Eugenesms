bae65287473006c882b672790ede98b7d77de258
Opilane TTHK <opilane@tthk.ee>
2025-11-27 09:44:54 +0200

    Loop

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Loop");
+
+            int loop = 20;
+
+            for (int i = 0; i < loop; i++)
+            {
+                Console.WriteLine("Loop" + i);
+                Console.Beep();
+            }
+        }
+    }
+}
