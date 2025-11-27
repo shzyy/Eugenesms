1de49f51433dcd91547f3c8a34e9fec54a83fbdc
Opilane TTHK <opilane@tthk.ee>
2025-11-27 11:44:12 +0200

    ref

---
+++ RefV0ut/RefV0ut/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace RefV0ut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ref vs Out");
+
+            int first = 1;
+            int second = 2;
+
+            method1(out first, out second);
+            method2(first,ref second);
+            method3(ref first, second);
+
+            Console.WriteLine($"first; {first}, second: {second}");
+            Console.ReadLine();
+        }
+
+        static void method1(out int a, out int b) 
+        {
+            a = 2;
+            b = 1;
+        }
+
+
+        static void method2(int a, ref int b)
+        {
+            a *= 5;
+            b *= a;
+        }
+
+        static void method3(ref int a, int b)
+        {
+            a *= 5;
+            b *= b;
+        }
+    }
+}
--- SulgudeKasutamine/SulgudeKasutamine/Program.cs
+++ SulgudeKasutamine/SulgudeKasutamine/Program.cs
@@ -7,9 +7,17 @@
             Console.WriteLine("Sulgude kasutamine");
             Console.WriteLine("Kui palju on: 3 + 8 / (4-2) * 4 = ?");
 
-            double calculation = 3 + 8 / (4 - 2) * 4;
+            double calculation1 = 3 + 8 / (4 - 2) * 4;
+            double calculation2 = 3 + 8 / 4 - 2 * 4;
+            double calculation3 = 3 + 8 / 4 - 2 * 4;
+            double calculation4 = 3 + 8 / ((4 - 2) * 4);
 
-            Console.WriteLine("vastus: " + calculation);
+            Console.WriteLine(" 3 + 8 / (4 - 2) * 4: " + calculation1);
+            Console.WriteLine(" 3 + 8 / 4 - 2 * 4: " + calculation2);
+            Console.WriteLine(" (3 + 8) / 4 - 2 * 4: " + calculation3);
+            Console.WriteLine(" 3 + 8 / ((4 - 2) * 4): " + calculation4);
+
+
         }
     }
 }
