dae26a1daea099676096d05b055e7b30ffbb2151
Opilane TTHK <opilane@tthk.ee>
2025-12-04 09:39:37 +0200

    w

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ForLoopEndless
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Loop Endless");
+            ushort i = 0;
+
+
+            for (; ; )
+            {
+                Console.WriteLine("Number " + i);
+                i++;
+
+
+                if (i == 11)
+                {
+                    break;
+                }
+
+
+
+            }
+        }
+    }
+}
