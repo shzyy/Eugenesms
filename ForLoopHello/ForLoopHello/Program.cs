42089042756e78db30336975ae6e1c3055bcecb6
Opilane TTHK <opilane@tthk.ee>
2025-12-04 10:01:44 +0200

    Tere

---
+++ ForLoopHello/ForLoopHello/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ForLoopHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Tere Tulemst!");
+            }
+
+        }
+    }
+}
