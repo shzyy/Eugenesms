5263680a232a201f9940d1840e4d4c792695005b
Opilane TTHK <opilane@tthk.ee>
2025-12-11 10:55:23 +0200

    FLT

---
+++ FLT/FLT/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FLT
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Tärnidest tehtud kolmnurk e poolik püramiid:");
+            Console.WriteLine("============================================");
+            Console.Write("Sisesta ridade arv: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+}
+}
