5c4615cb7c4601406e6199ee2dc2900cbfe32f4f
Opilane TTHK <opilane@tthk.ee>
2025-12-11 10:41:39 +0200

    FRMT

---
+++ FRMT/FRMT/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FRMT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Korrutustabel");
+
+            int j, i, n;
+
+            Console.WriteLine("sisesta tabelisse number, millest soovid alustada");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Korrutustabel ühest kun {0} ", n);
+            Console.WriteLine("\n");
+
+            for (i = 1; i <= n; i++) ;
+            {
+                for (j = 1; j <= n; j++)
+                {
+                    Console.WriteLine("{0}x{1} = {2},", j, i, i * j);
+                }
+            }
+    
+          
+            
+        }
+    }
+}
