7cc7979674fdd2d355257d115e41e4cb3793e25d
Opilane TTHK <opilane@tthk.ee>
2025-12-11 11:38:26 +0200

    FLPM

---
+++ FLPM/FLPM/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace FLPM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+      
+            {
+                Console.WriteLine("Tärnidest tehtud romb:");
+                Console.WriteLine("======================");
+                Console.Write("Sisesta ridade arv: ");
+
+                int n = int.Parse(Console.ReadLine());
+
+                
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= n - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int j = 1; j <= 2 * i - 1; j++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    Console.WriteLine();
+                }
+
+                
+                for (int i = n - 1; i >= 1; i--)
+                {
+                    for (int j = 1; j <= n - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int j = 1; j <= 2 * i - 1; j++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("\nValmis!");
+                Console.ReadKey();
+            }
+        }
+
+    }
+
+        
+
+    
+
+
