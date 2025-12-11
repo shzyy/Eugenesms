0319c7068d8f2292ca7822b5043c5362b7fb3489
Opilane TTHK <opilane@tthk.ee>
2025-12-11 11:17:18 +0200

    FLP

---
+++ FLP/FLP/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FLP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tärnidest tehtud püramiid:");
+            Console.WriteLine("==========================");
+            Console.Write("Sisesta ridade arv: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                
+                for (int j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nValmis!");
+            Console.ReadKey();
+        }
+    }
+}
