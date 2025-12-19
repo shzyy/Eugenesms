ebc12132c74b975c5e997d86bc09ba8f58ed15ec
Opilane TTHK <opilane@tthk.ee>
2025-12-19 13:09:45 +0200

    Circle

---
+++ DoRadius/DoRadius/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace DoRadius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Radius");
+
+            double radius;
+            double thickness = 0.4;
+            char symbol = '*';
+
+            do
+            {
+                Console.WriteLine("Enter radius:::: ");
+                if (!double.TryParse(Console.ReadLine(), out radius) || radius <= 0)
+                {
+                    Console.WriteLine("Radius have to be positive number");
+                }
+            }
+            while (radius <= 0);
+            Console.WriteLine();
+            double rIn = radius - thickness, rOut = radius + thickness;
+
+            for (double y = radius; y >= -radius; --y)
+            {
+                for (double x = -radius; x <rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.WriteLine(symbol);
+                    }
+                    else
+                    {
+                        Console.WriteLine(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
