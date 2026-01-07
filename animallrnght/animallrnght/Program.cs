9b6d79f710b369efd0804810e808ceedfc1df988
Opilane TTHK <opilane@tthk.ee>
2026-01-07 13:39:39 +0200

    Фаа

---
+++ animallrnght/animallrnght/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace animallrnght
+{
+    internal class Program
+    {
+        static int NimePikkus(string loomanimi)
+        {
+            return loomanimi.Length;
+        }
+
+        static void Main()
+        {
+           
+            string[] loomad = { "koer", "kass", "jänes", "sipelgasiil", "lendorav", "hakkas" };
+
+            string pikimNimi = "";
+            int maxPikkus = 0;
+
+            
+            foreach (string loom in loomad)
+            {
+                int pikkus = NimePikkus(loom);
+                Console.WriteLine($"{loom} pikkus: {pikkus}");
+
+                
+                if (pikkus > maxPikkus)
+                {
+                    maxPikkus = pikkus;
+                    pikimNimi = loom;
+                }
+            }
+
+            
+            Console.WriteLine($"\nKõige pikem loomanimi on: {pikimNimi}");
+        }
+    }    }
