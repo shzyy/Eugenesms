08e2a5bd55721e4c528e657e5757f2b621695a36
Opilane TTHK <opilane@tthk.ee>
2026-01-07 14:48:43 +0200

    3

---
+++ ForLoopArray/ForLoopArray/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ForLoopArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Creator creator = new Creator();
+            creator.CreateArray();
+
+            Console.ReadLine();
+        }
+
+        // Внутренний класс
+        class Creator
+        {
+            public void CreateArray()
+            {
+                string word = "Hello";
+                string[] array = new string[word.Length];
+
+                // Используем цикл
+                for (int i = 0; i < word.Length; i++)
+                {
+                    array[i] = word[i].ToString();
+                }
+
+                // Вывод массива
+                foreach (string s in array)
+                {
+                    Console.Write(s + " ");
+                }
+            }
+        }
+    }
+}
