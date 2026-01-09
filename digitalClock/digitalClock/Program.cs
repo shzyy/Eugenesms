0551136d482481411ba4f6046280895cc1a6455e
Opilane TTHK <opilane@tthk.ee>
2026-01-09 12:54:43 +0200

    Clock

---
+++ digitalClock/digitalClock/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace digitalClock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "digital clock";
+            Console.CursorVisible = false;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("digital Clock\n");
+                Console.WriteLine(DateTime.Now.ToString("HH:mm: ss"));
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
