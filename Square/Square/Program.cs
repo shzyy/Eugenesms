8bda7b4f8e732241b3255d0b756136aeccab8afd
Opilane TTHK <opilane@tthk.ee>
2026-01-09 14:54:05 +0200

    ый

---
+++ Square/Square/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta ruudu suurus:");
+            int size = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < size; i++)   
+            {
+                for (int j = 0; j < size; j++)    
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }    }
