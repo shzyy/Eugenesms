07e4f4ec8b878f8e7ad9c7336927cc13b9f6381a
Opilane TTHK <opilane@tthk.ee>
2026-01-09 13:43:32 +0200

    cc

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -7,16 +7,21 @@
             Console.Title = "digital clock";
             Console.CursorVisible = false;
 
-            while (true);
+            while (true) ;
             {
+                //puhastab konsooli akna
                 Console.Clear();
+                //võtab praeguse kellaaja ja vormindab selle kujule tunnid:minutid:sekundid
                 string time = DateTime.Now.ToString("HH:mm:ss");
-
+                //arvutab X-koordinaadi, et tekst oleks horisontaalselt keskel
                 int x = (Console.WindowWidth - time.Length) / 2;
+                //arvutab Y-koordinaadi (Vertikaalne keskkoht
                 int y = Console.WindowHeight / 2;
-
-              Console.SetCursorPosition(x, y);
+                //seab kursori arvutatud asukohta
+                Console.SetCursorPosition(x, y);
+                //kuvab praeguse kellaaja konsoolis
                 Console.Write(time);
+                //peatab programmi töö 1 sekundiks
                 Thread.Sleep(1000);
             }
         }
