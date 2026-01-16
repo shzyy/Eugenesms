36af848a591a1b92012ba833b10cd408e3e3e65c
Katerina2010dev <Katerina2simonova@gmail.com>
2026-01-16 15:45:10 +0200

    WordFinder

---
+++ FindWord/FindWord/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FindWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta otsitav sõna");
+            Console.WriteLine("kasuta formaati: s--a. kus - tähistab tundmatut tähte");
+
+            string choice = Console.ReadLine();
+
+            WordFinder(choice);
+
+        }
+        private static void WordFinder(string choice)
+        {
+            Regex regularExp = new Regex(@"\b" + choice.Replace("-", "\\w"),
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:/Users/opilane/Desktop/lemmad.txt", Encoding.Default))
+                {
+                    int lineNumber = 0;
+                    while (sr.EndOfStream == false)
+                    {
+                        string line = sr.ReadLine();
+
+                        if (line.Length == choice.Length)
+                        {
+                            MatchCollection matchCollection = regularExp.Matches(line);
+
+                            if (matchCollection.Count > 0)
+                            {
+                                lineNumber++;
+                                Console.WriteLine(lineNumber + " " + line);
+                            }
+                        }
+                    }
+
+                    sr.Close();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                {
+                    Console.WriteLine("ei ole olemas sellise pikkusega sõna");
+                    Console.WriteLine(ex.Message);
+                    throw;
+                }
+            }
+        }
+    }
+}
+
+
