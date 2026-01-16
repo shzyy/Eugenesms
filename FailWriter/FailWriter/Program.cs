afcee1f46989e790afa6034205a230ca4632da83
Katerina2010dev <Katerina2simonova@gmail.com>
2026-01-16 15:05:09 +0200

    FileWriter

---
+++ FailWriter/FailWriter/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FailWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta faili läbi konsooli");
+
+            string filepath = @"C:/Users/opilane/Desktop/cheshki.txt";
+            string inputText = Console.ReadLine();
+
+            File.WriteAllText(filepath, inputText);
+        }
+    }
+}
